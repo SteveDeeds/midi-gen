5593a74fecaf3b0e23b25828b3b2c2a453f21319
SteveDeeds <stevendeeds@yahoo.com>
2023-08-18 15:44:19 -0700

    Update gen_drums.py
    added more drums to drums_probabilistic

--- gen_drums.py
+++ gen_drums.py
@@ -11,14 +11,20 @@ def drums_two_step(scale, mf:MIDIFile, chords, volume, track):
             mf.addNote(track, track, 42, bar*4+beat+0.5, .5, volume//2) # 42 = CHH
 
 def drums_probabilistic(scale, mf:MIDIFile, chords, volume, track):
-            #1    e    +    a    2    e    +    a    3   e     +    a    4    e    +    a
-    kick_p =  [1,   0,   0,   0,   0.5, 0,   0,   0,   0.8,   0,   0,   0,   0.5, 0,   0,   0]
-    snare_p = [0,   0,   0,   0,   0.8,   0,   0,   0,   0.5, 0,   0,   0,   1,   0,   0,   0]
+
+    tom_notes = [41,43,45,47,48,50]
+
+              #1    e    +    a    2    e    +    a    3    e    +    a    4    e    +    a
+    kick_p =  [1,   0.2, 0.4, 0,   0.8, 0,   0,   0,   0.5, 0,   0,   0,   0.5, 0,   0,   0.2]
+    snare_p = [0,   0,   0,   0,   0.5, 0,   0,   0.2, 1,   0.2, 0.5, 0,   0.8, 0, 0.3,   0]
     chh_p =   [0.8, 0.4, 0.6, 0.4, 0.8, 0.4, 0.6, 0.4, 0.8, 0.4, 0.6, 0.4, 0.8, 0.4, 0.6, 0.4]
+    tom_p =   [0,   0,   0,   0,   0,   0,   0,   0,   0.3, 0.1, 0,   0,   0.6, 0.1, 0.9, 0.1]
 
     kick = np.zeros((16))
     snare = np.zeros((16))
     chh = np.zeros((16))
+    crash = np.zeros((16))
+    tom = np.zeros((16))
 
     for sixteenth in range(16):
         if random.random() < kick_p[sixteenth]:
@@ -27,15 +33,40 @@ def drums_probabilistic(scale, mf:MIDIFile, chords, volume, track):
             snare[sixteenth] = 1
         if random.random() < chh_p[sixteenth]:
             chh[sixteenth] = 1
+        if random.random() < tom_p[sixteenth]:
+            tom[sixteenth] = 1
+    
+    if(random.random()<0.9):
+        crash[0] = 1
+        if(random.random()<0.5):
+            crash[4] = 1
+            if(random.random()<0.5):
+                crash[8] = 1
+    
+    # change some of the closed high hats to open high hats
+    # print(tom)
+    ohh = []
+    for i, sixteenth in enumerate(chh):
+        if random.random() < 0.2:
+            ohh.append(sixteenth)
+            chh[i] = 0
+        else:
+            ohh.append(0)
 
     for bar, chord in enumerate(chords):
         for sixteenth in range(16):
             if kick[sixteenth] > 0:
-                mf.addNote(track, track, 35, bar*4+sixteenth/4, .5, volume) # 35 = kick
+                mf.addNote(track, track, 36, bar*4+sixteenth/4, .5, volume) # 36 = electronic kick (also C1)
             if snare[sixteenth] > 0:
                 mf.addNote(track, track, 38, bar*4+sixteenth/4, .5, volume) # 38 = snare
             if chh[sixteenth] > 0:
                 mf.addNote(track, track, 42, bar*4+sixteenth/4, .5, volume) # 42 = CHH
+            if ohh[sixteenth] > 0:
+                mf.addNote(track, track, 46, bar*4+sixteenth/4, .5, volume) # 46 = OHH
+            if crash[sixteenth] > 0 and bar==0:
+                mf.addNote(track, track, 49, bar*4+sixteenth/4, .5, volume) # 49 = crash
+            if tom[sixteenth] > 0 and (bar==3 or bar==7):
+                mf.addNote(track, track, random.choice(tom_notes), bar*4+sixteenth/4, .5, volume) 
 
 def drum_subdivided(scale, mf:MIDIFile, chords, volume, track):
     subdivisions = []
